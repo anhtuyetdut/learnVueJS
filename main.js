b4b52cb7d696eff446e72c91de5b6475950046fe
anhtuyetdut <anhtuyetdutk19@gmail.com>
2022-09-12 23:16:29 +0700

    v-on

--- main.js
+++ main.js
@@ -3,16 +3,33 @@ var vueInstance = new Vue({
     data: {
         title: 'samsung',
         url:'https://v2.vuejs.org/v2/guide/instance.html',
-        number: 20
+        number: 20,
+        clientX: 0,
+        clientY: 0
     },
     methods: {
         say: function(text) {
             return `Hello ` + text;
         },
+        //JS expression
         chooseYesNo(){
             var number = this.number;
             console.log(number);
             return number > 10 ?`YES`: `NO`;
+        },
+        //event
+        handleClick(event) {
+            console.log(`click`, event);
+            this.number += 1;
+        },
+        handleClickWithNumber(event, count){
+            console.log(`click`, event);
+            this.number += count;
+        },
+        handleMouseMove(event){
+            console.log(event);
+            this.clientX = event.clientX;
+            this.clientY =event.clientY;
         }
     }
 });
