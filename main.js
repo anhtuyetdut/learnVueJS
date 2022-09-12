8eeb05a6f9fdba03686426e00d96b905dde83bde
anhtuyetdut <anhtuyetdutk19@gmail.com>
2022-09-12 15:31:31 +0700

    data binding one-way and JS expression

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var vueInstance = new Vue({
     el: '#app',
     data: {
-        title: 'samsung'
+        title: 'samsung',
+        url:'https://v2.vuejs.org/v2/guide/instance.html',
+        number: 20
     },
     methods: {
         say: function(text) {
             return `Hello ` + text;
+        },
+        chooseYesNo(){
+            var number = this.number;
+            console.log(number);
+            return number > 10 ?`YES`: `NO`;
         }
     }
 });
